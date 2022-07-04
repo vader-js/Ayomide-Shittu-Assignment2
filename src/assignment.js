7a9ede550978b8381cf5b77b4aa787b9d09384bf
Ayomide Shittu <100224192+vader-js@users.noreply.github.com>
2022-07-04 17:42:56 +0000

    called a reduce() function for the first challenge in order to get the sum of Numbers. used a filter() function on the second challenge to pass only the even Number in an Array of Numbers.

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,9 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.reduce((a, b) => a + b , 0);
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -28,9 +28,9 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.filter( a => a % 2 == 0);
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
 
 /**
  * Challenge - 3
